06b0699528d4671e9035c04f98f44e7d2e2b99cb
kasperrey <kasper.reynders@gmail.com>
2023-12-20 14:12:32 +0100

    eerste commit

--- accelerometer.js
+++ accelerometer.js
@@ -1,8 +1,8 @@
-var x = 100;
-var y = 100;
-var x_v = true;
-var y_v = true;
-var positionBal = {"x": 0, "y": 0, "z": 0};
+let x = 100;
+let y = 100;
+let x_v = true;
+let y_v = true;
+let positionBal = {"x": 0, "y": 0, "z": 0};
 
 const muren_str = [
   [".", ".", ".", ".", ".", ".", ".", ".", ".", "#", ".", ".", ".", ".", ".", ".", "."],
@@ -24,18 +24,18 @@ const muren_str = [
   [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."]
 ];
 
-var muren = [];
+let muren = [];
 
 
 class Botsen {
   botsMetRectCircel(x1, y1, x2, y2, x_r, y_r, r) {
-    var dist_x = (x1+5 - x_r);
-    var dist_y = (y1+5 - y_r);
-    var distance = Math.sqrt((dist_x * dist_x) + (dist_y * dist_y));
-    var een_x = dist_x/distance;
-    var een_y = dist_y/distance;
-    var point_x = (x_r+(een_x*r));
-    var point_y = (y_r+(een_y*r));
+    let dist_x = (x1+5 - x_r);
+    let dist_y = (y1+5 - y_r);
+    let distance = Math.sqrt((dist_x * dist_x) + (dist_y * dist_y));
+    let een_x = dist_x/distance;
+    let een_y = dist_y/distance;
+    let point_x = (x_r+(een_x*r));
+    let point_y = (y_r+(een_y*r));
     return this.botsMetRectPoint(x1, y1, x2, y2, point_x, point_y);
   }
 
@@ -63,9 +63,9 @@ class Muur {
 
 
 function kijkInBuurt(pos_x, pos_y) {
-  var bal_x = Math.floor(pos_x/10);
-  var bal_y = Math.floor(pos_y/10);
-  var lijst = [];
+  let bal_x = Math.floor(pos_x/10);
+  let bal_y = Math.floor(pos_y/10);
+  let lijst = [];
   for (let y = Math.max(0, bal_y-1); y <= Math.min(bal_y+1, muren.length-1); y++) {
     let rij = muren[y];
     for (let x = Math.max(0, bal_x-1); x <= Math.min(bal_x+1, rij.length-1); x++) {
@@ -78,8 +78,8 @@ function kijkInBuurt(pos_x, pos_y) {
 }
 
 function isGebotst(pos_x, pos_y) {
-  var muren_in_buurt = kijkInBuurt(pos_x, pos_y);
-  for (var muur of muren_in_buurt) {
+  let muren_in_buurt = kijkInBuurt(pos_x, pos_y);
+  for (let muur of muren_in_buurt) {
     if (botsen.botsMetRectCircel(muur.x1, muur.y1, muur.x2, muur.y2, pos_x, pos_y, 4)) {
       return [true, muren_in_buurt];
     }
@@ -88,12 +88,12 @@ function isGebotst(pos_x, pos_y) {
 }
 
 function changeX(xyz) {
-  var nieuwe_x = x-(xyz.x * 10);
-  var nieuwe_y = y-(xyz.y * 10);
+  let nieuwe_x = x-(xyz.x * 10);
+  let nieuwe_y = y-(xyz.y * 10);
 
-  var isgebotst = isGebotst(nieuwe_x, nieuwe_y);
+  let isgebotst = isGebotst(nieuwe_x, nieuwe_y);
   if (!isgebotst[0]) {
-    var b = botsen.botsAchtergrond(nieuwe_x, nieuwe_y, 4);
+    let b = botsen.botsAchtergrond(nieuwe_x, nieuwe_y, 4);
     x_v = b[0];
     y_v = b[1];
     if (!x_v)
@@ -107,10 +107,10 @@ function changeX(xyz) {
 
 function draw() {
   g.clearRect(x-4, y-4, x+4, y+4);
-  var muren_opnieuw = changeX(positionBal);
+  let muren_opnieuw = changeX(positionBal);
   g.fillCircle(x, y, 4);
   g.setColor(0, 0, 0);
-  for (var muur of muren_opnieuw)
+  for (let muur of muren_opnieuw)
     g.fillRect(muur.x1, muur.y1, muur.x2, muur.y2);
   g.setColor(255, 0, 0);
 }
@@ -120,7 +120,7 @@ function maakMuren() {
     let rij_str = muren_str[y];
     let rij = [];
     for (let x = 0; x < rij_str.length; x++) {
-      if (rij_str[x] == "#") {
+      if (rij_str[x] === "#") {
         rij.push(new Muur(x, y));
       } else {
         rij.push(null);
@@ -141,8 +141,8 @@ Bangle.on('accel', function(xyz) {
 
 g.clear();
 g.setColor(0, 0, 0);
-for (var rij of muren) {
-  for (var muur of rij) {
+for (let rij of muren) {
+  for (let muur of rij) {
     if (muur)
       g.fillRect(muur.x1, muur.y1, muur.x2, muur.y2);
   }
